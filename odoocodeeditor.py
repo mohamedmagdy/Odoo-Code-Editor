1c5e4a0ad8a550970a60a3415f8e25c15df6c9ec
Mohamed Magdy <moh.magdy40@gmail.com>
2016-11-25 17:43:22 +0200

    [ADD] Add the main text window and the main menus.

--- odoocodeeditor.py
+++ odoocodeeditor.py
@@ -10,9 +10,35 @@ class Main(QtGui.QMainWindow):
         self.initUI()
 
     def initUI(self):
+        self.text = QtGui.QTextEdit(self)
+        self.setCentralWidget(self.text)
+
+        self.initToolbar()
+        self.initMenubar()
+
+        # Initialize a statusbar for the window
+        self.statusbar = self.statusBar()
+
         # X and Y coordinates on the screen, width, height
         self.setGeometry(100, 100, 1030, 800)
         self.setWindowTitle("Odoo Code Editor")
+
+    def initToolbar(self):
+
+        self.toolbar = self.addToolBar("Options")
+
+        # Makes the next toolbar appear underneath this one
+        self.addToolBarBreak()
+
+    def initMenubar(self):
+        menubar = self.menuBar()
+
+        file = menubar.addMenu("File")
+        edit = menubar.addMenu("Edit")
+        view = menubar.addMenu("View")
+        tools = menubar.addMenu("Tools")
+        help = menubar.addMenu("Help")
+
 
 def main():
     app = QtGui.QApplication(sys.argv)
